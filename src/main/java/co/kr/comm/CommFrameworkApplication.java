19f3702da769a87bef6c462b72aa51cb8a86d813
다미 <ekal0323@gmail.com>
2026-01-28 22:39:00 +0900

    [feature] init

---
+++ src/main/java/co/kr/comm/CommFrameworkApplication.java
@@ -0,0 +1,13 @@
+package co.kr.comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kr/comm/CommFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.comm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
