7a3a83315f35674143968ca4e2a635ccf2933be1
ekal0323 <ekal0323@lgupluspartners.co.kr>
2026-02-01 19:36:26 +0900

    [feature] log4j2 설정 및 환경별 설정 분리

--- src/main/java/co/kr/comm/CommFrameworkApplication.java
+++ src/main/java/co/kr/comm/CommFrameworkApplication.java
@@ -1,13 +1,18 @@
 package co.kr.comm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommFrameworkApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(CommFrameworkApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CommFrameworkApplication.class, args);
+		logger.info("[CommFrameworkApplication.main] test 중");
     }
 
 }
--- src/test/java/co/kr/comm/CommFrameworkApplicationTests.java
+++ src/test/java/co/kr/comm/CommFrameworkApplicationTests.java
@@ -1,13 +1,22 @@
 package co.kr.comm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class CommFrameworkApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void triggerRolling() throws InterruptedException {
+		for (int i = 0; i < 2000; i++) {
+			log.debug("롤링 테스트를 위한 대량 로그 생성 중... 번호: {}", i);
+			log.info("현재 시간: {}", System.currentTimeMillis());
+			// 용량 기반 롤링을 보고 싶다면 Thread.sleep 없이 실행
+			// 시간 기반을 보고 싶다면 약간의 간격을 줌
+			Thread.sleep(10);
+		}
+	}
 
 }
